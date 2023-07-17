72dd407cc2eb6e83557fa49eb2332b330012a3fb
breakeryu <35825642+breakeryu@users.noreply.github.com>
2023-07-17 08:13:15 +0800

    完成GPIO

--- Class4_GPIO/SPL_Test/Src/main.c
+++ Class4_GPIO/SPL_Test/Src/main.c
@@ -37,17 +37,14 @@ int main(void)
     */
   sys_SWJ_REMAP_Config(SWJ_ENABLE);
 
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  BSP_GPIO_Init();
+  
 
 
   while (1)
   {
-    /* code */
+    BSP_GPIO_Write_Demo();
   }
   
 }
---
+++ Class5_TIM/SPL_Test/BSP/bsp_rcc.c
@@ -0,0 +1,100 @@
+
+#include "bsp_rcc.h"
+
+
+/* Public variables ---------------------------------------------------------*/
+uint32_t _mcu_rev_id;
+uint32_t _mcu_dev_id;
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+RCC_ClocksTypeDef RCC_ClockFreq;
+ErrorStatus HSEStartUpStatus;
+
+/**
+  * @brief  Configures Vector Table base location.
+  * @param  None
+  * @retval None
+  */
+void BSP_RCC_NVIC_Configuration(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  /* Enable and configure RCC global IRQ channel */
+  NVIC_InitStructure.NVIC_IRQChannel = RCC_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;  
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+
+
+
+
+void BSP_RCC_Init(void)
+{
+  /* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
+  SystemInit();
+
+  /* Configure the System Peripheral clock prescalers */
+  BSP_RCC_Periph_Clock_Init();
+
+  //Update SystemCoreClock variable according to Clock Register Values
+  SystemCoreClockUpdate();
+
+  /* This function fills the RCC_ClockFreq structure with the current
+  frequencies of different on chip clocks (for debug purpose) */
+  RCC_GetClocksFreq(&RCC_ClockFreq);
+
+  /* NVIC configuration ------------------------------------------------------*/
+  //BSP_RCC_NVIC_Configuration();
+
+  delay_init(72);
+
+  // _mcu_rev_id = DBGMCU_GetREVID();
+  // _mcu_dev_id = DBGMCU_GetDEVID();
+
+}
+
+
+void BSP_RCC_Periph_Clock_Init(void)
+{
+
+
+
+  //ADC CLK Prescaler, ADC CLK = 72 / 6 = 12 Mhz
+  RCC_ADCCLKConfig(RCC_PCLK2_Div6);
+  //USB OTG CLK FS Prescaler, when PLL = 72Mhz, OTGFSPRE = 0
+  RCC_USBCLKConfig(RCC_USBCLKSource_PLLCLK_1Div5);
+  //MCO 
+  RCC_MCOConfig(RCC_MCO_NoClock);
+
+
+  //Clear Clock interrupt register
+  RCC->CIR = 0;
+
+  //Reset APB2 and APB1 Peripheral
+  RCC->APB2RSTR = 0;
+  RCC->APB1RSTR = 0;
+
+  //AHB Peripheral CLK Enable
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_SRAM,ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_FLITF,ENABLE);
+
+  //APB2 Peripheral CLK Enable
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+
+  //APB1 Peripheral CLK Enable
+  RCC->APB1ENR = 0;
+
+  //RTC CLK
+  RCC_LSEConfig(RCC_LSE_ON);
+  RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI);
+  RCC_RTCCLKCmd(ENABLE);
+
+}
+
