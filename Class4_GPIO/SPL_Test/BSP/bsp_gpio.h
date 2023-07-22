0fe11eb4211bbd586dba6e30948348f3c291a768
breakeryu <35825642+breakeryu@users.noreply.github.com>
2023-07-22 19:30:52 +0800

    更新GPIO的驱动。更新Class4_GPIO

--- Class4_GPIO/SPL_Test/BSP/bsp_gpio.h
+++ Class4_GPIO/SPL_Test/BSP/bsp_gpio.h
@@ -6,9 +6,65 @@
 #include "delay.h"
 
 
+//位带操作
+//IO口操作宏定义
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2))
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr))
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum))
+//IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08
+
+//IO口操作,只对单一的IO口!
+//确保n的值小于16!
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+
+
 
 void BSP_GPIO_Init(void);
 void BSP_GPIO_Write_Demo(void);
+
+void BSP_GPIO_SetCfg(GPIO_TypeDef *gp, uint16_t index,GPIOSpeed_TypeDef speed, GPIOMode_TypeDef mode);
+uint16_t BSP_GPIO_GetGPIO_InSt(GPIO_TypeDef *gp);
+uint8_t BSP_GPIO_GetGPIO_InPortSt(GPIO_TypeDef *gp, uint8_t index);
+uint16_t BSP_GPIO_GetGPIO_OutSt(GPIO_TypeDef *gp);
+uint8_t BSP_GPIO_GetGPIO_OutPortSt(GPIO_TypeDef *gp, uint8_t index);
+void BSP_GPIO_SetGPIO_OutSt(GPIO_TypeDef *gp, uint16_t val);
+void BSP_GPIO_SetGPIO_OutPortSt(GPIO_TypeDef *gp, uint8_t index, uint8_t val);
+uint8_t BSP_GPIO_SetGPIO_Lock(GPIO_TypeDef *gp, uint16_t val);
+
 
 #endif//__BSP_GPIO_H
 
